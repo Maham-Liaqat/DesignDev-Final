33e2b4acb6377941d01bdb8fa2036b6136fcb154
Maham-Liaqat <mahamliaqat1234@gmail.com>
2025-07-26 04:57:37 +0500

    Fix route order for /profile/by-username endpoint

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -12,11 +12,11 @@ userRoute.post("/forgot-password", forgotPassword)
 userRoute.post("/reset-password", resetPassword)
 
 // Profile routes
-userRoute.get("/profile/:userId", getUserProfile)
 userRoute.get("/profile/by-username/:username", getUserByUsername)
-userRoute.put("/profile", upload.single('profileImage'), Authentication, updateUserProfile)
+userRoute.get("/profile/by-email/:email", getUserByEmail)
 userRoute.get("/profile/:userId/stats", getUserTemplateStats)
 userRoute.get("/profile/:userId/comprehensive-stats", getUserComprehensiveStats)
-userRoute.get("/profile/by-email/:email", getUserByEmail)
+userRoute.get("/profile/:userId", getUserProfile)
+userRoute.put("/profile", upload.single('profileImage'), Authentication, updateUserProfile)
 
 module.exports=userRoute
